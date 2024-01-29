909482aaa483d562e68b71eeb00fc1e6c685af51
KirillShytov <sitovk587@gmail.com>
2024-01-29 14:46:07 +0200

    ccc

--- src/main.js
+++ src/main.js
@@ -13,28 +13,76 @@ const input = document.querySelector('.search-input');
 const startBtn = document.querySelector('.search-button');
 const loader = document.querySelector('.loader');
 
+loader.style.display = 'none';
+form.addEventListener('submit', handleSearch);
+
+function handleSearch(event) {
+  event.preventDefault();
+  loader.style.display = 'block';
+  const inputValue = event.target.elements.query.value;
+  fetchImage(inputValue)
+    .then(data => {
+      loader.style.display = 'none';
+      if (!data.hits.length) {
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+      container.innerHTML = '';
+      container.innerHTML = createMarkup(data.hits);
+      const refreshPage = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+      refreshPage.refresh();
+      form.reset();
+    })
+    .catch(onFetchError);
+}
+
+function fetchImage(name) {
+  const url = `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=18`;
+
+  return fetch(url).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
+}
+
 function createMarkup(arr) {
   return arr
     .map(
       ({
         webformatURL,
-        largeImageUrl,
+        largeImageURL,
         tags,
         likes,
         views,
         comments,
         downloads,
-      }) =>
-        `<li class="gallery-item">
-      <a class="gallerry-link" href="${largeImageUrl}">
-      <img
+      }) => `
+  <li class="gallery-item">
+  <a class="gallery-link" href="${largeImageURL}">
+    <img
       class="gallery-image"
       src="${webformatURL}"
       alt="${tags}"
-      />
-      <p class="gallery-description">• Likes: ${likes} • Views: ${views} • Comments: ${comments} • Downloads:${downloads}</p>
-      </a>
-      </li>`
+    />
+    <p class= "gallery-descr">• Likes: ${likes} • Views: ${views} • Comments: ${comments} •</span> Downloads:${downloads}</p>
+  </a>
+</li>
+  `
     )
     .join('');
+}
+
+function onFetchError(error) {
+  iziToast.error({
+    title: 'Error',
+    message: 'OOps... Plese try again',
+  });
 }
